cb10424660d1b10e223aab2176b28c46f4525eaf
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-12-18 19:05:41 +0000

    toString Test

--- Main.java
+++ Main.java
@@ -1,49 +1,33 @@
 import java.util.Scanner;
 
-public class Main
-{
-  public static void main(String[] args)
-  {
+public class Main {
+  public static void main(String[] args) {
     Student test = new Student(1, 2, "Bob");
     System.out.println(test);
-    Plane plane = new Plane();
-    System.out.println(plane.getLoc());
-    // Uncomment and complete the code below to test your Plane class
-    /* Delete this line to uncomment code **************************
     Scanner sc = new Scanner(System.in);
     String input = "";
     Plane p = new Plane(1000);
-    while (!input.equals("q"))
-    {
-      System.out.println(p);
+    while (!input.equals("q")) {
       System.out.println("Type \"u\" to move upwards, \"d\" to move downwards, \"n\" for new Plane, \"q\" to quit.");
       // System.out.println(print the plane's location and uncomment);
       input = sc.nextLine();
-
-      if (input.equals("u"))
-      {
-        // move the plane upwards
-      }
-      else if (input.equals("d"))
-      {
+      if (input.equals("u")){
+        p.upward();
+        System.out.print(p.toString());
+      } else if (input.equals("d")) {
+        p.downward();
+        System.out.print(p.toString());
         // move the plane downwards
-      }
-      else if (input.equals("n"))
-      {
+      } else if (input.equals("n")) {
         System.out.println("Starting location for a new plane?");
         int location = sc.nextInt();
         sc.nextLine(); // consume the ENTER key left behind by nextInt()
-        p = // create new plane at proper location
-      }
-      else if (input.equals("q"))
-      {
+        p = new Plane(location); // create new plane at proper location
+      } else if (input.equals("q")) {
         System.out.println("Thank you for using plane simulator");
-      }
-      else
-      {
+      } else{
         System.out.println("Invalid input");
       }
     }
-    Delete this line to uncomment ************************************/
   }
 }
--- Plane.java
+++ Plane.java
@@ -10,4 +10,29 @@ public class Plane
   public int getLoc() {
     return this.location;
   }
+  public void upward() {
+      if((this.location > 2000) || (this.location + 100 > 2000)) {
+        System.out.println("You cannot go past 2000");
+        this.location = 2000;
+      } else {
+        this.location += 100;
+      }
+  }
+  public void downward() {
+    if((this.location < -2000) || (this.location - 100 < -2000)) {
+      this.location = -2000;
+      System.out.println("You cannot go past -2000");
+    } else {
+      this.location -= 100;
+    }
+  }
+  public String toString() {
+      int spaces = location - (-2000); 
+      String result = ""; // empty string
+      for (int i = 0; i < spaces; i++) {
+          result += " ";
+      }
+      result += "@";
+      return result;
+  }
 }
